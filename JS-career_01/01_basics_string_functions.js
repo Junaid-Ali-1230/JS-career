1b6780d70bf72816152a68a9abeca764536c7631
Junaid-Ali-1230 <91932484+Junaid-Ali-1230@users.noreply.github.com>
2025-02-25 11:29:50 +0000

    container config done

---
+++ JS-career_01/01_basics_string_functions.js
@@ -0,0 +1,23 @@
+// console.log("Junaid is here!")
+
+let name = "Junaid"
+// let wife = "zunaira"
+// let target = "System"
+// console.log(`My name is ${name}. I am married to ${wife} and our target is building a ${target}`)
+// console.log(typeof(name))
+
+// ***************************** String Object *****************************
+const  gameName = new String ("abcdLemXseneXiopj")
+// console.log(gameName[8])
+// console.log(gameName.__proto__)
+// console.log(gameName.length)
+// console.log(gameName.toLowerCase())  
+// console.log(gameName.trim())
+// console.log(gameName.substring(0,4))   //strating/ending index.
+// console.log(gameName.slice(-8, 9))
+// console.log(gameName.replace('X',"-R-"))
+// console.log(gameName.replaceAll('X',"-R-"))
+// console.log(gameName.includes("Lem"))
+// console.log(gameName.split("X")) 
+
+
